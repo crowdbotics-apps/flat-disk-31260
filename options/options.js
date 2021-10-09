9027fd0ee6d35a8e03dd9c59bde3b6f7d9a3276a
Crowdbotics <team@crowdbotics.com>
2021-10-09 08:20:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "flat_disk_31260",
+  url: "https://flat_disk_31260.botics.co",
+  api: "https://flat_disk_31260.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
